a1cd49b827585a94695a568bb33018c644c86c89
Valera <IsDevSign@gmail.com>
2022-07-07 10:12:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 22, 44, 33, 55, 77, 12, 3, 7 };
+            InsertionSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {   
+            for (int i = 1; i < array?.Length; i++)
+            {
+                int currentIndex = array[i];
+                int previousIndex = i - 1;
+
+                while (previousIndex >= 0 && array[previousIndex] > currentIndex)
+                {
+                    array[previousIndex + 1] = array[previousIndex];
+                    previousIndex--;
+                }
+                array[previousIndex + 1] = currentIndex;
+            }
+        }
+    }
+}
